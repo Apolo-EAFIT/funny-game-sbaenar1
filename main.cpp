c799fa90e26e6b8a63c1ec76bb1edcb44e07a26a
Santiago Baena <sbaenar1@eafit.edu.co>
2016-05-10 11:11:44 -0500

    main of funny game

---
+++ main.cpp
@@ -0,0 +1,48 @@
+//
+// Created by Santiago Baena Rivera on 10/05/16.
+//
+#include <iostream>
+
+using namespace std;
+
+bool game(){
+    bool winner;
+
+    return winner;
+}
+
+void checkWins(int winnerAirpot, bool winner){
+    (winner == true ? cout << "First player wins flying to airport" << winnerAirpot : cout << "First player loses") << endl;
+}
+
+int main() {
+    int nroAero = 0;
+    int start = 0;
+
+    cin >> nroAero >> start;
+
+    int aero[nroAero][nroAero];
+
+    for (int i = 0; i < nroAero; ++i) {
+        for (int j = 0; j < nroAero; ++j) {
+            aero[i][j] = 0;
+        }
+    }
+
+    for (int s = 0; s < (nroAero-1); ++s){
+        int arist1 = 0;
+        int arist2 = 0;
+        cin >> arist1 >> arist2;
+        aero[arist1-1][arist2-1] = 1;
+        aero[arist2-1][arist1-1] = 1;
+    }
+    for(int z=0; z < nroAero; ++z) {
+        for (int h = 0; h < nroAero; ++h) {
+            cout << aero[z][h];
+        }
+        cout << endl;
+    }
+
+
+    return 0;
+}
