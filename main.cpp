684613b826f8e3a5cd41b713f1dc985c6c61fb36
Santiago Baena <sbaenar1@eafit.edu.co>
2016-05-11 09:35:33 -0500

    mejorando el codigo(aun sin resultados)

--- main.cpp
+++ main.cpp
@@ -5,15 +5,12 @@
 
 using namespace std;
 
-bool game(){
-    bool winner;
+void game(int tam, int arcos[tam], bool conec[tam], int comienzo);
 
-    return winner;
-}
 
-void checkWins(int winnerAirpot, bool winner){
-    (winner == true ? cout << "First player wins flying to airport" << winnerAirpot : cout << "First player loses") << endl;
-}
+void checkWins(int winnerAirpot, bool winner);
+
+
 
 int main() {
     int nroAero = 0;
@@ -22,10 +19,12 @@ int main() {
     cin >> nroAero >> start;
 
     int aero[nroAero][nroAero];
+    bool visited[nroAero][nroAero];
 
     for (int i = 0; i < nroAero; ++i) {
         for (int j = 0; j < nroAero; ++j) {
             aero[i][j] = 0;
+            visited[i][j]=false;
         }
     }
 
@@ -33,16 +32,32 @@ int main() {
         int arist1 = 0;
         int arist2 = 0;
         cin >> arist1 >> arist2;
-        aero[arist1-1][arist2-1] = 1;
-        aero[arist2-1][arist1-1] = 1;
+        aero[arist1-1][arist2-1] = arist2;
+        aero[arist2-1][arist1-1] = arist1;
+        visited[arist1-1][arist2-1]= true;
+        visited[arist2-1][arist1-1] = true;
     }
     for(int z=0; z < nroAero; ++z) {
         for (int h = 0; h < nroAero; ++h) {
-            cout << aero[z][h];
+            cout << visited[z][h];
         }
         cout << endl;
     }
 
+    game(nroAero,aero[nroAero],visited[nroAero],start);
 
     return 0;
+}
+
+void game(int tam, int arcos[tam], bool conec[tam], int comienzo) {
+    bool winner = false;
+    int l = 0;
+
+
+
+    checkWins(l, winner);
+}
+
+void checkWins(int winnerAirpot, bool winner) {
+    (winner ? cout << "First player wins flying to airport" << winnerAirpot : cout << "First player loses") << endl;
 }
